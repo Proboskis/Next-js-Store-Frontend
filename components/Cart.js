cb930d3c60434ca7310e4a3a7752418187e47850
Proboskis <nikolasurla22@gmail.com>
2022-09-02 13:45:23 +0200

    9. Adding total price and quantities in the cart, some minor styling changes.

--- components/Cart.js
+++ components/Cart.js
@@ -1,11 +1,11 @@
 import {useStateContext} from "../lib/context";
-import {CartWrapper, CartStyle, Card, CardInfo, EmptyStyle} from "../styles/CartStyles";
+import {CartWrapper, CartStyle, Card, CardInfo, EmptyStyle, Checkout} from "../styles/CartStyles";
 import {FaShoppingCart} from 'react-icons/fa';
 import {AiFillMinusCircle, AiFillPlusCircle} from "react-icons/ai"
 import {Quantity} from "../styles/ProductDetails";
 
 export default function Cart() {
-    const {cartItems, setShowCart, onAdd, OnRemove} = useStateContext();
+    const {cartItems, setShowCart, onAdd, OnRemove, totalPrice} = useStateContext();
     return(
         <CartWrapper onClick={() => setShowCart(false)}>
             <CartStyle onClick={(e) => e.stopPropagation()}>
@@ -22,7 +22,7 @@ export default function Cart() {
                                 <img src={item.image.data.attributes.formats.thumbnail.url} alt={item.title} />
                                 <CardInfo>
                                     <h3>{item.title}</h3>
-                                    <h3>{item.price}</h3>
+                                    <h3>{item.price}$</h3>
                                     <Quantity>
                                         <span>Quantity</span>
                                         <button onClick={() => OnRemove(item)}>
@@ -37,6 +37,12 @@ export default function Cart() {
                             </Card>
                         );
                     })}
+                {cartItems.length >= 1 && (
+                    <Checkout>
+                        <h3>Subtotal: {totalPrice.toFixed(2)}$</h3>
+                        <button>Purchase</button>
+                    </Checkout>
+                )}
             </CartStyle>
         </CartWrapper>
     );
