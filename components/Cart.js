ac1d324200a6baa87941752d5d683ce2b5304d61
Proboskis <nikolasurla22@gmail.com>
2022-09-05 14:01:49 +0200

    11. Staggering animations within the cart, using object variables, and also adding the animation in the Nav component to animate the shopping cart icon number when adding items to the cart.

--- components/Cart.js
+++ components/Cart.js
@@ -1,8 +1,25 @@
 import {useStateContext} from "../lib/context";
-import {CartWrapper, CartStyle, Card, CardInfo, EmptyStyle, Checkout} from "../styles/CartStyles";
+import {CartWrapper, CartStyle, Card, CardInfo, EmptyStyle, Checkout, Cards} from "../styles/CartStyles";
 import {FaShoppingCart} from 'react-icons/fa';
 import {AiFillMinusCircle, AiFillPlusCircle} from "react-icons/ai"
 import {Quantity} from "../styles/ProductDetails";
+
+// Animation Variants
+const card = {
+    hidden: {opacity: 0, scale: 0.8},
+    show: {opacity: 1, scale: 1}
+}
+
+const cards = {
+    hidden: {opacity: 1},
+    show: {
+        opacity: 1,
+        transition: {
+            delayChildren: 0.4,
+            staggerChildren: 0.15,
+        },
+    },
+};
 
 export default function Cart() {
     const {cartItems, setShowCart, onAdd, OnRemove, totalPrice} = useStateContext();
@@ -15,10 +32,11 @@ export default function Cart() {
                         <FaShoppingCart />
                     </EmptyStyle>
                 )}
+                <Cards variants={cards} initial="hidden" animate="show">
                 {cartItems.length >= 1 &&
                     cartItems.map((item) => {
                         return (
-                            <Card animate={{opacity: 1, scale: 1}} initial={{opacity: 0, scale: 0.8}} transition={{delay: 0.3}} key={item.slug}>
+                            <Card variants={card} key={item.slug}>
                                 <img src={item.image.data.attributes.formats.thumbnail.url} alt={item.title} />
                                 <CardInfo>
                                     <h3>{item.title}</h3>
@@ -37,6 +55,7 @@ export default function Cart() {
                             </Card>
                         );
                     })}
+                </Cards>
                 {cartItems.length >= 1 && (
                     <Checkout>
                         <h3>Subtotal: {totalPrice.toFixed(2)}$</h3>
